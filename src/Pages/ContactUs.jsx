03d69b52008e3a1767bbe683a0acd436eeb57410
Muktar <muktaradamu677@gmail.com>
2026-04-02 15:50:50 +0100

    upd flex-col in contact us page

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -12,7 +12,7 @@ const ContactUs = () => {
           " At Beiroot, we are passionate about bringing the vibrant flavors."
         }
       />
-      <section className="max-w-[1440px] flex flex-row-reverse flex-col justify-center  lg:flex-row lg:flex-nowrap mx-auto w-full scroll-0 py-16 px-4 md:px-6 lg:px-8 flex  gap-12 md:gap- lg:gap-4">
+      <section className="max-w-[1440px] flex flex-col-reverse flex-col justify-center  lg:flex-row lg:flex-nowrap mx-auto w-full scroll-0 py-16 px-4 md:px-6 lg:px-8 flex  gap-12 md:gap- lg:gap-4">
         {" "}
         <Form className="flex-1" />
         {/*  */}
